bcf7cd741e1fc16c301402f8779405cafc9410ba
Rom4nB <romensch_2@yahoo.de>
2017-06-08 13:22:51 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// the name of your app as used in the click package (required)
+var webappName = "handyticketdeutschland.rom4nb"
+
+// the start url of your app (required)
+var webappUrl = "https://handyticket.de/portals/web/x/system/login.html?proc=neutral"
+
+// the the pattern that defines which links are considered
+// local and which are opened in an external browser (required)
+var webappUrlPattern = "http?://www.handyticket.de/*,http?://handyticket.de/*,,https://handyticket.de/portals/web/x/system/6b4f66003b2a794d7a6b58531a137a3b286e4b61.continue"
+
+// a user agent override (optional)
+// var webappUA = "uncomment this line and set your User Agent string here between these quotes, if you need to override it"
+
+// Haptic feedback for links (note, this does not work if your site
+// uses javascript functions to open links) (optional)
+// var hapticLinks = "true"
+
+// Audible feedback when clicking links (the same constraints as
+// for hapticLinks apply) (optional)
+// var audibleLinks = "true"
